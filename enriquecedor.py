1b52a668d983b732943a56bbeae4baa8335ecd6c
Felipe Dioguardi <felipedioguardi@gmail.com>
2021-06-07 03:21:20 -0300

    feat: queries both wikidata and dbpedia
    This is are some awful queries, don't look at them

--- enriquecedor.py
+++ enriquecedor.py
@@ -1,6 +1,7 @@
 from rdflib import Graph, Namespace
+from rdflib.namespace import OWL
 from sys import argv, exit
-from SPARQLWrapper import SPARQLWrapper
+from SPARQLWrapper import SPARQLWrapper, XML
 
 SCHEMA = Namespace("https://schema.org/")
 HOMEBREW = Namespace(
@@ -31,21 +32,65 @@ def get_persons(graph):
 def request_query(url, query):
     sparql = SPARQLWrapper(url)
     sparql.setQuery(query)
+    sparql.setReturnFormat(XML)
     return sparql.query().convert()
 
 
 def query_wikidata(name):
-    query = """
-    SELECT DISTINCT ?person ?predicate ?object
-    WHERE {
-       ?person wdt:P31|wdt:P279 wd:Q5;
-              ?label "{}".
-       ?person ?predicate ?object.
-    } LIMIT 1
+    query = (
     """
+    CONSTRUCT {
+      ?person ?predicate ?object
+    }
+    WHERE {
+      ?person wdt:P31|wdt:P279 wd:Q5;
+              ?label "%s";
+              ?predicate ?object.
+    }
+    """
+        % name
+    )
 
-    return Graph().parse(
-        data=request_query("https://query.wikidata.org/sparql", query.format(name)))
+    return request_query("https://query.wikidata.org/sparql", query)
+
+
+def query_dbpedia(name):
+    query = (
+        """
+    CONSTRUCT {
+      ?person ?predicate ?object
+    }
+    WHERE {
+      ?person rdf:type dbo:Person;
+              dbo:birthName|foaf:name ?name;
+              ?predicate ?object.
+      FILTER(REGEX(?name, "%s", "e")).
+    }
+    """
+        % name
+    )
+
+    return request_query("http://dbpedia.org/sparql", query)
+
+
+def get_subject(graph):
+    query = graph.query(
+        """
+        SELECT DISTINCT ?subject
+        WHERE {
+          ?subject?predicate ?object
+        } LIMIT 1
+        """
+    )
+    for subject in query:
+        return subject[0]
+
+
+def merge_graphs(graph, external_graph, person):
+    if len(external_graph) > 0:
+        graph += external_graph
+        graph.add((get_subject(external_graph), OWL.sameAs, person))
+    return graph
 
 
 def main():
@@ -58,8 +103,8 @@ def main():
     output = load_input(argv[1])
 
     for person, name in get_persons(output):
-        output += query_wikidata(name.toPython())
-            # + query_dbpedia(name.toPython())
+        graph = merge_graphs(output, query_wikidata(name.toPython()), person)
+        graph = merge_graphs(output, query_dbpedia(name.toPython()), person)
 
     print(output.serialize(format="turtle").decode("utf-8"))
 
